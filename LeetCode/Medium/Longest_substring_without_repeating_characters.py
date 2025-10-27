2b83529dcb2eada3cdba0fa7da1f875015a75860
sumer <mysumer123@gmail.com>
2025-10-27 19:57:28 +0000

    Day1, solved 2 questions

---
+++ LeetCode/Medium/Longest_substring_without_repeating_characters.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+      char_index = {}
+      # write down the position where string last shown up.
+      left = 0
+      ans = 0
+
+      for right in range(len(s)):
+        current_char = s[right]
+
+        # if the string already exist in the window, move the left
+        if current_char in char_index and char_index[current_char] >= left:
+          left = char_index[current_char] + 1
+
+        # update the position of string
+        char_index[current_char] = right
+
+        # update the answer
+        ans = max(ans, right - left + 1)
+
+      return ans
